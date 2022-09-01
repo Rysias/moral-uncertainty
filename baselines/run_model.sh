9e27befba9b97b2dd2a5f0291704d78c20f246f4
Rysias <jhvithamar@gmail.com>
2022-09-01 14:50:06 +0000

    change to bert

--- baselines/run_model.sh
+++ baselines/run_model.sh
@@ -1,1 +1,1 @@
-python tune.py --ngpus 1 --model microsoft/deberta-v3-large --learning_rate 1e-5 --batch_size 16 --nepochs 4 --gradient_acc_steps 1 --verbose --save
+python tune.py --ngpus 1 --model bert-base-uncased --learning_rate 3e-5 --batch_size 16 --nepochs 4 --gradient_acc_steps 1 --verbose --save
